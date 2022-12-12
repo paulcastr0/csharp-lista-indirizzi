b59418e7a0b4a7e04dfc5d7b4c6b7fd3778b56be
paulcastr0 <castropaul.learn@gmail.com>
2022-12-12 16:37:24 +0100

    Aggiungere i file di progetto.

---
+++ ListaIndirizzi/Indirizzo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaIndirizzi
+{
+    public class Indirizzo
+    {
+        private string name;
+        private string surname;
+        private string street;
+        private string city;
+        private string state;
+        private string zip;
+
+        public Indirizzo(string name, string surname, string street, string city, string state, string zip)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.street = street;
+            this.city = city;
+            this.state = state;
+            this.zip = zip;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+        public string GetSurname()
+        {
+            return surname;
+        }
+        public string GetString()
+        {
+            return street;
+        }
+        public string GetCity()
+        {
+            return city;
+        }
+        public string GetState()
+        {
+            return state;
+        }
+        public string GetZip()
+        {
+            return zip;
+        }
+
+        public override string ToString()
+        {
+            return $"L'indirizzo di {name} {surname} è {street} {city} {state} {zip}"; 
+        }
+    }
+}
---
+++ ListaIndirizzi/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ListaIndirizzi;
+
+List<Indirizzo> listaIndirizzi = new List<Indirizzo>();
+
+try
+{
+    StreamReader file = File.OpenText("C:\\Users\\Generation\\Desktop\\Nuova cartella\\OOP-Csharp\\ListaIndirizzi\\Indirizzi.txt");
+    file.ReadLine();
+    while (!file.EndOfStream)
+    {
+        string riga = file.ReadLine();
+
+
+        string[] informazioniIndirizzo = riga.Split(",");
+
+        string name = informazioniIndirizzo[0];
+        string surname = informazioniIndirizzo[1];
+        string street = informazioniIndirizzo[2];
+        string city = informazioniIndirizzo[3];
+        string state = informazioniIndirizzo[4];
+        string zip = informazioniIndirizzo[5];
+
+        Indirizzo indirizzoEstratto = new Indirizzo(name,surname,street,city,state,zip);
+        listaIndirizzi.Add(indirizzoEstratto);
+
+    }
+    file.Close();
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
+
+
+foreach (Indirizzo indirizzo in listaIndirizzi)
+{
+    Console.WriteLine(indirizzo);
+}
+
